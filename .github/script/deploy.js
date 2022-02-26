4cffe7cdffeef6289bb134d4bd78966004bb26c5
CN-traveler <cn-traveler@qq.com>
2022-02-26 15:13:23 +0800

    更新脚本

--- .github/script/deploy.js
+++ .github/script/deploy.js
@@ -16,7 +16,7 @@ fs.readFile('_config.next.yml', function (error, data) {
     if (error) {
         console.log('read error')
     } else {
-        fs.writeFile('_config.next.yml', data.toString().replace(/google_site_verification: Lx6KtXmKbORXXoEMyg8SFmQVmvL6OK9X_AMBBnnHfMc/g, "google_site_verification: 2b9bRX_y9bcOfteSamVfvKl04A2-h4autV9BpH8tfyA"), function (error) {
+        fs.writeFile('_config.next.yml', data.toString().replace(/client_id: e307f849f27945af2561/g, "client_id: 96723ff736e1241622df").replace(/client_secret: 0434ef190d2e76ba8270e1e082efb45cf4977586/g, "client_secret: 6d720153fa6643dc6d20c87452e5e12b6dab3e85").replace(/google_site_verification: Lx6KtXmKbORXXoEMyg8SFmQVmvL6OK9X_AMBBnnHfMc/g, "google_site_verification: 2b9bRX_y9bcOfteSamVfvKl04A2-h4autV9BpH8tfyA"), function (error) {
             if (error) {
                 console.log('write error')
             } else {
